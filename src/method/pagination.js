2b6335da0135abb5781bae6f5f3c3f64f7550fcd
Antonio <quadjovie.antonio@gmail.com>
2024-04-28 00:18:48 +0200

    clean

--- src/method/pagination.js
+++
@@ -1,6 +0,0 @@
- function paginate (array, pageSize, pageNumber) {
-     const startIndex = (pageNumber - 1) * pageSize;
-     return array.slice(startIndex, startIndex + pageSize);
-
-}
-export {paginate}
